b009a827c630e1667125c57966b67b83232d1240
Arthur Leones <arthur_leones@hotmail.com>
2022-01-26 12:55:00 -0300

    Basic features completed

---
+++ __init__.py
@@ -0,0 +1,53 @@
+from urllib.request import urlopen
+from urllib.parse import urlencode
+import json
+
+class Experiment:
+    def __init__(self, ip, port=8080):
+        self.address = f'http://{ip}:{port}/'
+        self.update_config()
+
+    def start(self):
+        '''Starts the experiment'''
+        response = self.api('control?cmd=start')
+        return response['result']
+
+    def stop(self):
+        '''Stops the experiment'''
+        response = self.api('control?cmd=stop')
+        return response['result']
+
+    def clear(self):
+        '''Clear all buffers'''
+        response = self.api('control?cmd=clear')
+        return response['result']
+
+    def update_config(self):
+        '''Updates the known experiment configuration'''
+        config = self.api('config')
+        self.crc32 = config['crc32']
+        self.title = config['title']
+        self.localTitle = config['localTitle']
+        self.category = config['category']
+        self.localCategory = config['localCategory']
+        self.bufferNames = []
+        for b in config['buffers']:
+            self.bufferNames.append(b['name'])
+        return
+
+    def get(self, buffers=None, threshold=None):
+        '''Retrieves buffer data'''
+        if buffers is None:
+            buffers = self.bufferNames
+        if threshold is None:
+            threshold = 'full'
+        params = {}
+        for b in buffers:
+            params[b] = str(threshold)
+        params = urlencode(params, safe='|')
+        return self.api('get?' + params)
+
+    def api(self, cmd):
+        '''Sends a generic command'''
+        response = urlopen(self.address + cmd)
+        return json.loads(response.read().decode())
